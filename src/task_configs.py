e12b6c11cea376f99e09bae25915204a69f5b0b5
Jack Speat <speatjack@gmail.com>
2025-04-28 15:38:19 +0100

    fixed dataset not found error and xdg cache

--- src/task_configs.py
+++ src/task_configs.py
@@ -39,7 +39,7 @@ def prompt_generation(data_name, data_item, args):
         prompt = data_item["instruction"]
     elif data_name in ["wildbench_v2-hard"]:
         prompt = data_item["conversation_input"][0]["content"]
-    elif data_name in ["zebra-grid", "zebra-grid-mini"]:
+    elif data_name in ["zebra-grid", "zebra-grid-mini", "zebra-grid-test"]:
         prompt = apply_lgp_grid_template(data_item, format=args.format) 
     elif data_name in ["gsm", "math-l5"]:
         question_key = "question"
